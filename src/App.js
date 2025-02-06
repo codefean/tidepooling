b944cae3b7987f54aadf82cbd41890cc9ae8d68f
codefean <124543425+codefean@users.noreply.github.com>
2025-02-06 14:43:46 -0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TidepoolingApp from "./pages/TidepoolingApp";
 import "./pages/TidepoolingApp.css";
 
-
 function App() {
+  useEffect(() => {
+    document.title = "Anna Tidepools"; // Set the tab title
+  }, []);
+
   return (
     <div className="App">
       <TidepoolingApp />
